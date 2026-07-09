a082604e6ab424e89cbf7cc361729a51702fe46e
hanbyeol.yang <beuri97@gmail.com>
2026-07-09 23:31:32 +0900

    init: initial commit

---
+++ src/main/java/dev/nbcsparta/assignment/nbccoffeeordersystem/NbcCoffeeOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.nbcsparta.assignment.nbccoffeeordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcCoffeeOrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcCoffeeOrderSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/nbcsparta/assignment/nbccoffeeordersystem/NbcCoffeeOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nbcsparta.assignment.nbccoffeeordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcCoffeeOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
